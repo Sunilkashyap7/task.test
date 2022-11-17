bda5289ad22b256cbb7be244d5966d9c5502bd2b
Sunilkashyap7 <118354565+Sunilkashyap7@users.noreply.github.com>
2022-11-17 10:00:12 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,16 @@
+let shopItemsData = [
+  {
+    id: "first",
+    name: "Casual Shirt",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "second",
+    name: "Mens Suit",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+];
